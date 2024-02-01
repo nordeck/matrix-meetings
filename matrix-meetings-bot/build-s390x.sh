908e39518cb46bad40739a02a82c54420094262e
Milton Moura <miltonmoura@gmail.com>
2024-02-01 18:38:19 +0000

    Update image build process to use latest rust crypto node bindings with s390x support
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>

--- matrix-meetings-bot/build-s390x.sh
+++ matrix-meetings-bot/build-s390x.sh
@@ -1,27 +1,14 @@
-#!/bin/bash
-
-source /root/.nvm/nvm.sh
-
-# get the deps we need to build the bot
-apt-get install --no-install-recommends -y curl git rustc cargo vim python3
-
-# get the rust crypto nodejs bindings from our fork branch
-git clone --depth 1 --branch nic/fix/s390x https://github.com/nordeck/matrix-rust-sdk-crypto-nodejs.git /build/matrix-sdk-crypto-nodejs
-cd /build/matrix-sdk-crypto-nodejs
-yarn link
-yarn install
-yarn build
+#!/bin/sh
 
 # get the matrix bot sdk from our fork branch
-git clone --depth 1 --branch nic/fix/s390x https://github.com/nordeck/matrix-bot-sdk.git /build/matrix-bot-sdk
+git clone --depth 1 --branch v0.7.0-s390x-nic-fix https://github.com/nordeck/matrix-bot-sdk.git /build/matrix-bot-sdk
 cd /build/matrix-bot-sdk
-yarn link "@matrix-org/matrix-sdk-crypto-nodejs"
 yarn link
 yarn install --ignore-optional
 yarn build
 
 # prepare the matrix-meetings bot
+# TODO: update to main when merging
 git clone --depth 1 --branch nic/feat/bot-e2ee https://github.com/nordeck/matrix-meetings.git /build
 cd /build
-yarn link "@matrix-org/matrix-sdk-crypto-nodejs"
 yarn link "matrix-bot-sdk"
