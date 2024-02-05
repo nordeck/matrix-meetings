3315902ce155a4345ffa9e7bb39d26e1d58719cf
Milton Moura <miltonmoura@gmail.com>
2024-02-05 11:15:19 +0100

    Fixing matrix-bot-sdk dep version
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>

--- matrix-meetings-bot/build-s390x.sh
+++ matrix-meetings-bot/build-s390x.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # get the matrix bot sdk from our fork branch
-git clone --depth 1 --branch v0.7.0-s390x-nic-fix https://github.com/nordeck/matrix-bot-sdk.git /build/matrix-bot-sdk
+git clone --depth 1 --branch v0.7.1-s390x-nic-fix https://github.com/nordeck/matrix-bot-sdk.git /build/matrix-bot-sdk
 cd /build/matrix-bot-sdk
 yarn link
 yarn install --ignore-optional
