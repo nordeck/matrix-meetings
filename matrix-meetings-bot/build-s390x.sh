8ce5113040b9410cd124015af33d2edb0f0c7b86
Milton Moura <miltonmoura@gmail.com>
2024-02-05 14:54:53 +0000

    Add the rust crypto node bindings package link for correct resolution
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>

--- matrix-meetings-bot/build-s390x.sh
+++ matrix-meetings-bot/build-s390x.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# get the rust crypto node bindings
+# we don't need to build them anymore but still need them locally for package linking
+git clone --depth 1 --branch v0.1.0-beta.12 https://github.com/matrix-org/matrix-rust-sdk-crypto-nodejs.git /build/matrix-sdk-crypto-nodejs
+cd /build/matrix-sdk-crypto-nodejs
+yarn link
+yarn install
+
 # get the matrix bot sdk from our fork branch
 git clone --depth 1 --branch v0.7.1-s390x-nic-fix https://github.com/nordeck/matrix-bot-sdk.git /build/matrix-bot-sdk
 cd /build/matrix-bot-sdk
 yarn link
+yarn link @matrix-org/matrix-sdk-crypto-nodejs
 yarn install --ignore-optional
 yarn build
 
@@ -11,4 +19,5 @@ yarn build
 # TODO: update to main when merging
 git clone --depth 1 --branch nic/feat/bot-e2ee https://github.com/nordeck/matrix-meetings.git /build
 cd /build
+yarn link @matrix-org/matrix-sdk-crypto-nodejs
 yarn link "matrix-bot-sdk"
