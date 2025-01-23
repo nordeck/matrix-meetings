f6b9057669c8232d8dc48e0d1f5b901182af1352
Christian Paul <christian.paul@nordeck.net>
2025-01-23 11:11:38 +0100

    Wrap env vars into double quotes
    Signed-off-by: Christian Paul <christian.paul@nordeck.net>

--- charts/matrix-meetings-bot/files/shell-tools/create_bot_account.sh
+++ charts/matrix-meetings-bot/files/shell-tools/create_bot_account.sh
@@ -3,11 +3,11 @@ set -x;
 while [ $(curl -k -sw '%{http_code}' "$HOMESERVER" -o /dev/null) -ne 302 ]; do
   sleep 1;
 done
-response=$(curl -k --write-out '%{http_code}' --silent --output /dev/null -X GET --header 'Accept: application/json' $HOMESERVER/_matrix/client/r0/register/available?username=$USERTOCREATE)
+response=$(curl -k --write-out '%{http_code}' --silent --output /dev/null -X GET --header 'Accept: application/json' "$HOMESERVER/_matrix/client/r0/register/available?username=$USERTOCREATE")
 if [ "$response" = 400 ]; then
     echo "Bot user already exists"
 else
   echo "Will create User $USERTOCREATE on $HOMESERVER"
-  register_new_matrix_user -a -u $USERTOCREATE -p $BOT_PASSWORD -c /data/homeserver.yaml $HOMESERVER
+  register_new_matrix_user -a -u "$USERTOCREATE" -p "$BOT_PASSWORD" -c /data/homeserver.yaml "$HOMESERVER"
 fi
 exit 0
