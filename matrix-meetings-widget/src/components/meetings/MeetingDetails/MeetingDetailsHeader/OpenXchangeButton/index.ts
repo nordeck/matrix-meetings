48a554150da1afbfbfcb94f3dda15433e13eb634
Michael Weimann <michael.weimann@nordeck.net>
2025-02-13 11:40:51 +0100

    Fix prettier formatting

--- matrix-meetings-widget/src/components/meetings/MeetingDetails/MeetingDetailsHeader/OpenXchangeButton/index.ts
+++ matrix-meetings-widget/src/components/meetings/MeetingDetails/MeetingDetailsHeader/OpenXchangeButton/index.ts
@@ -14,5 +14,5 @@
  * limitations under the License.
  */
 
-export { OpenXchangeButton } from './OpenXchangeButton';
 export { getOpenXChangeExternalReference } from './openXchange';
+export { OpenXchangeButton } from './OpenXchangeButton';
