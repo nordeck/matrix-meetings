64c66bbf5f19544cbe5eff9253efe8a0455f0791
Michael Weimann <michael.weimann@nordeck.net>
2025-02-26 09:50:02 +0100

    Add helm lint (#767)
    Signed-off-by: Michael Weimann <michael.weimann@nordeck.net>
    Signed-off-by: MTRNord <mtrnord1@gmail.com>
    Co-authored-by: MTRNord <mtrnord1@gmail.com>

--- charts/matrix-meetings-bot/files/shell-tools/create_bot_account.sh
+++ charts/matrix-meetings-bot/files/shell-tools/create_bot_account.sh
@@ -1,13 +1,13 @@
-#/bin/sh
+#!/bin/sh
 set -x;
-while [ $(curl -k -sw '%{http_code}' "$HOMESERVER" -o /dev/null) -ne 302 ]; do
+while [ "$(curl -k -sw '%{http_code}' "$HOMESERVER"/_matrix/client/versions -o /dev/null)" -ne 200 ]; do
   sleep 1;
 done
-response=$(curl -k --write-out '%{http_code}' --silent --output /dev/null -X GET --header 'Accept: application/json' $HOMESERVER/_matrix/client/r0/register/available?username=$USERTOCREATE)
+response=$(curl -k --write-out '%{http_code}' --silent --output /dev/null -X GET --header 'Accept: application/json' "$HOMESERVER/_matrix/client/r0/register/available?username=$USERTOCREATE")
 if [ "$response" = 400 ]; then
     echo "Bot user already exists"
 else
   echo "Will create User $USERTOCREATE on $HOMESERVER"
-  register_new_matrix_user -a -u $USERTOCREATE -p $BOT_PASSWORD -c /data/homeserver.yaml $HOMESERVER
+  register_new_matrix_user -a -u "$USERTOCREATE" -p "$BOT_PASSWORD" -c /data/homeserver.yaml "$HOMESERVER"
 fi
 exit 0
